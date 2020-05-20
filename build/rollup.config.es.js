de271b91448011f300997b93405f744e1044068d
Daniil <daniil.chumachenko@democrance.com>
2020-05-20 13:20:22 +0400

    different build  + reduce

--- build/rollup.config.es.js
+++ build/rollup.config.es.js
@@ -3,7 +3,8 @@ import base from './rollup.config.base';
 const config = Object.assign({}, base, {
   output: {
     name: 'vue-tel-input',
-    file: 'dist/vue-tel-input.esm.js',
+    dir: 'dist',
+    // file: 'dist/vue-tel-input.esm.js',
     format: 'es',
   },
 });
